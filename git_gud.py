f5d67e56866878377700d2a782d6fda78a46a048
Nikolai Magnussen <nikolai@magnussen.tf>
2018-11-04 16:26:42 +0100

    Fix issues and make flake8 compliant

--- git_gud.py
+++ git_gud.py
@@ -7,6 +7,7 @@ from config import key
 from config import owners
 from github import Github, GithubException
 
+
 def print_help():
     '''
     Print help describing the syntax for how to use the program.
@@ -17,7 +18,8 @@ def print_help():
     Returns:
         - None
     '''
-    print("Usage {} <action> [--organization/-o=<organization>] <search string>".format(sys.argv[0]))
+    usage = f"{sys.argv[0]} <action> [--organization/-o=<org>] <search string>"
+    print(f"Usage: {usage}")
     print("Available actions:")
     print("    ls")
     print("    push-comment")
@@ -52,20 +54,22 @@ def is_matching(repo, project, organization):
 
     return False
 
-def git_add_commit_push(filename, cwd):
+
+def git_add_commit_push(result, cwd):
     '''
     Adds, commits and pushes file to remote.
 
     Parameters:
-        - Filename of the file to be added to the repo and pushed
+        - Result of the file to be added to the repo and pushed
         - Cwd is the directory path of the repo where the file is located
 
     Returns:
         - None
     '''
-    subprocess.run(["git", "add", result], cwd = cwd)
-    subprocess.run(["git", "commit", "-m", f"Graded project, see the {result}-file in the root directory"], cwd = cwd)
-    subprocess.run(["git", "push"], cwd = cwd)
+    commit_msg = f"Graded project, see the {result}-file in the root directory"
+    subprocess.run(["git", "add", result], cwd=cwd)
+    subprocess.run(["git", "commit", "-m", commit_msg], cwd=cwd)
+    subprocess.run(["git", "push"], cwd=cwd)
 
 
 def parse_markdown_grading_sheeet(filename):
@@ -80,7 +84,7 @@ def parse_markdown_grading_sheeet(filename):
         - None if a non-markdown file was provided
 
     '''
-    if not ".md" in filename:
+    if ".md" not in filename:
         return None
 
     grading_sheet = dict()
@@ -91,7 +95,7 @@ def parse_markdown_grading_sheeet(filename):
         for line in markdown_file:
             # New student, so add previous student to dict
             if line.startswith("### "):
-                if student_name != None:
+                if student_name is not None:
                     grading_sheet[student_name] = student_content
                 student_name = line.strip("# \n")
                 student_content = line
@@ -132,9 +136,8 @@ def add_commit_push_grading_sheet():
     '''
     filename = input("Enter path to sheet filename: ")
     grading_sheet = parse_markdown_grading_sheeet(filename)
-    if grading_sheet == None:
+    if grading_sheet is None:
         print(f"{filename} is not a markdown file: it must end in .md")
-
 
     project_dir = "{}/{}".format(os.getcwd(), project)
     repos = os.listdir(project_dir)
@@ -148,7 +151,8 @@ def add_commit_push_grading_sheet():
             for student_name in grading_sheet.keys():
                 if repo.endswith(student_name):
                     found_student = True
-                    print(f"Assuming {repo} belongs to {student_name}: adding grading comment")
+                    print(f"Assuming {repo} belongs to {student_name}: "
+                          "adding grading comment")
                     with open(f"{repo_dir}/{result}", "w+") as f:
                         f.write(grading_sheet[student_name])
                     break
@@ -158,7 +162,6 @@ def add_commit_push_grading_sheet():
                 print("Pushed changes to github")
             else:
                 print(f"Found no student matching repo name {repo}")
-
 
 
 def add_commit_push(project, comment):
@@ -187,7 +190,7 @@ def add_commit_push(project, comment):
         repo_dir = "{}/{}".format(project_dir, repo)
         if os.path.isdir(repo_dir):
             if not comment:
-                inp = input(f"Did {repo} pass or fail? Type 'fail' for fail. [Default: pass]: ")
+                inp = input(f"Did {repo} 'pass' or 'fail'? [Default: pass]: ")
                 if inp == "fail":
                     result = failed
                 elif inp == "skip":
@@ -208,6 +211,7 @@ def add_commit_push(project, comment):
         else:
             print(f"\n{repo} is not a directory, and can't be a repo")
 
+
 def list_matching(project, organization):
     '''
     Lists the repositories matching the provided project and organization.
@@ -223,6 +227,7 @@ def list_matching(project, organization):
     for repo in g.get_user().get_repos():
         if is_matching(repo, project, organization):
             print(repo.name)
+
 
 def set_matching_readonly(project, organization):
     '''
@@ -244,7 +249,7 @@ def set_matching_readonly(project, organization):
         if is_matching(repo, project, organization):
             print("Changing permissions for {}".format(repo.name))
             for collab in repo.get_collaborators():
-                if not collab.login in owners:
+                if collab.login not in owners:
                     # Student found. change permissions from push to pull
                     repo.remove_from_collaborators(collab)
                     try:
@@ -253,9 +258,11 @@ def set_matching_readonly(project, organization):
                     except GithubException as e:
                         print(e)
                         repo.add_to_collaborators(collab)
-                        print("    {} can still write because readonly is only possible for organizations".format(collab.login))
+                        print("    {} can still write because readonly is only"
+                              " possible in orgs".format(collab.login))
                 else:
                     print("    Owner: {}".format(collab.login))
+
 
 def clone_matching(project, organization):
     '''
@@ -275,8 +282,11 @@ def clone_matching(project, organization):
             if not os.path.isdir(project_dir):
                 os.mkdir(project_dir)
             split_idx = repo.clone_url.find("github.com")
-            repo_url = "{}{}@{}".format(repo.clone_url[:split_idx], key, repo.clone_url[split_idx:])
-            subprocess.run(["git", "clone", repo_url], cwd = project_dir)
+            repo_url = "{}{}@{}".format(repo.clone_url[:split_idx],
+                                        key,
+                                        repo.clone_url[split_idx:])
+            subprocess.run(["git", "clone", repo_url], cwd=project_dir)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -299,7 +309,8 @@ if __name__ == "__main__":
     elif action == "clone":
         clone_matching(project, organization)
     elif action == "set_readonly":
-        print("Are you sure you want to set all non-owners of the matching repos to read-only?")
+        print("Are you sure you want to set all non-owners of the matching "
+              "repos to read-only?")
         print("Type 'YES' to confirm")
         ans = input()
         if ans == "YES":
